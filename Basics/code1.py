fea05496cb62e271a41c3ee5f45fa828a9641d3b
Swarnima <124855325+oringejooz@users.noreply.github.com>
2025-01-19 22:05:15 +0530

    new file:   Basics/code1.py

---
+++ Basics/code1.py
@@ -0,0 +1,19 @@
+# Print
+print("HI,Swarnima")
+name = input("Input your name : ")
+print(f"Hi, {name} Welcome to learning python with Swarnima")
+
+# STRINGS
+
+first_n = "Swarnima"
+last_n = "Bisht"
+
+print(first_n+" "+last_n)
+
+# INT
+
+age = 21
+age+=1
+print(age)
+print(type(age))
+# Type function determines the 
